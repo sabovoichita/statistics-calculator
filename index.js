1cd29cbdd32712a964b72e188f20c6962edbab40
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-25 18:20:38 +0100

    16. Now you need to use your new getMean function. In your calculate function, declare a mean variable and assign it the value of getMean(numbers).

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const getMean = (array) =>
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map((el) => {
-    Number(el).filter((el) => {
-      !isNaN(el);
-    });
-  });
+  const numbers = array.map((el) => Number(el).filter((el) => !isNaN(el)));
+  const mean = getMean(numbers);
 };
