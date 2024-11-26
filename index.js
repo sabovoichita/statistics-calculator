cb3146bd2f3d9cf69ad4fcdcf0f02233c4799c48
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 13:48:22 +0100

    41. Thankfully, you can handle both of these cases at once with the .filter() method. Start by declaring a mode variable and assigning it the value of Object.keys(counts).

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const getMode = (array) => {
     return null;
   }
   const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter();
 };
 
 const calculate = () => {
