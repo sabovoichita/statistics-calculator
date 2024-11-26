a54672a54769e49c054c57df4648c2e7373ae16d
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 11:36:54 +0100

    29. Your next calculation is the mode, which is the number that appears most often in the list. To get started, declare a getMode function that takes the same array parameter you have been using.

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
-// const testArr1 = [1, 2, 3, 4, 5];
-// const testArr2 = [1, 2, 3, 4, 5, 6];
-// const isEven = testArr2.length % 2 === 0;
-// console.log(isEven);
-// const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-// console.log(oddListMedian);
-// const evenListMedian = getMean([
-//   testArr2[testArr2.length / 2],
-//   testArr2[testArr2.length / 2] - 1,
-// ]);
-// console.log(evenListMedian);
-
 const getMedian = (array) => {
   const sorted = array.toSorted((a, b) => a - b);
   const median =
@@ -21,6 +9,8 @@ const getMedian = (array) => {
       : sorted[Math.floor(sorted.length / 2)];
   return median;
 };
+
+const getMode = (array) => {};
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
