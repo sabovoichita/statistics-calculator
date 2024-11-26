5c08c521c4b62900f66c2355e8eeea28c6582976
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 15:46:15 +0100

    56. Declare a standardDeviation variable, and use the Math.pow() function to assign it the value of  variance1/2  .To calculate a root exponent, such as  x−−√n  , you can use an inverted exponent  x1/n  . JavaScript has a built-in Math.pow() function that can be used to calculate exponents. Here is the basic syntax for the Math.pow() function: Example Code Math.pow(base, exponent);

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
+  const standardDeviation = Math.pow(variance, 1 / 2);
 };
 
 const calculate = () => {
