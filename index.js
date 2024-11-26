7ed10f7077c490f525ce4329d0e8676bdde93b5f
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 15:33:30 +0100

    51. With two .map() calls and a .reduce() call, you're creating extra arrays and iterating more times than needed. You should move all of the logic into the .reduce() call to save time and memory.
    Remove the differences, squaredDifferences, and sumSquaredDifferences variables (and their values). Declare a variance variable, and assign it the value of array.reduce(). For the callback, pass in your standard acc and el parameters, but leave the function body empty for now. Don't forget to set the initial value to 0.

--- index.js
+++ index.js
@@ -28,12 +28,7 @@ const getRange = (array) => Math.max(...array) - Math.min(...array);
 
 const getVariance = (array) => {
   const mean = getMean(array);
-  const differences = array.map((el) => el - mean);
-  const squaredDifferences = differences.map((el) => el ** 2);
-  const sumSquaredDifferences = squaredDifferences.reduce(
-    (acc, el) => acc + el,
-    0
-  );
+  const variance = array.reduce((acc, el) => {}, 0);
 };
 
 const calculate = () => {
