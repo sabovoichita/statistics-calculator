1454649b9202b87d9b4873072285f154813c2995
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 15:48:55 +0100

    57. The Math object has a .sqrt() method specifically for finding the square root of a number. Change your standardDeviation variable to use this method instead of Math.pow().

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
-  const standardDeviation = Math.pow(variance, 1 / 2);
+  const standardDeviation = Math.sqrt(variance);
 };
 
 const calculate = () => {
